8f4a94e2e11fd26bd5994f21948db1227c6b5f67
Hannes Verschore <hv1989@gmail.com>
2025-11-10 21:48:03 +0100

    Problem 3607: trying macros for the first time to see if that helps.

--- problem3607/main.rs
+++ problem3607/main.rs
@@ -3,11 +3,14 @@ use std::collections::HashMap;
 use std::time::Instant;
 use std::time::Duration;
 
-struct Solution {
-}
+macro_rules! create_solver {
+    ($name:ident, $typename:ty) => {
 
-impl Solution {
-    pub fn find_station_group(group_ids: &mut Vec<i32>, station: i32) -> i32 {
+struct $name;
+
+impl $name
+{
+    pub fn find_station_group(group_ids: &mut Vec<$typename>, station: $typename) -> $typename {
         let gid = station as usize;
 
         if group_ids[gid] == -1 {
@@ -20,40 +23,36 @@ impl Solution {
         }
 
         let gid = group_ids[gid];
-        let root = Solution::find_station_group(group_ids, gid); 
+        let root = Self::find_station_group(group_ids, gid); 
         group_ids[gid as usize] = root;
         return root;
     }
-    pub fn process_queries(c: i32, connections: Vec<Vec<i32>>, queries: Vec<Vec<i32>>, elapsed: &mut Duration) -> Vec<i32> {
+
+    pub fn process_queries(c: i32, connections: Vec<Vec<i32>>, queries: Vec<Vec<i32>>) -> Vec<i32> {
         let c = c as usize + 1;
 
-        let mut groups : HashMap<i32, Vec<i32>> = HashMap::new();
-        let mut group_ids : Vec<i32> = Vec::with_capacity(c);
+        let mut groups : HashMap<$typename, Vec<$typename>> = HashMap::new();
+        let mut group_ids : Vec<$typename> = Vec::with_capacity(c);
         group_ids.resize(c, -1);
 
-        let now = Instant::now();
-
         for con in connections {
-            let root0 = Solution::find_station_group(&mut group_ids, con[0]);
-            let root1 = Solution::find_station_group(&mut group_ids, con[1]);
+            let root0 = Self::find_station_group(&mut group_ids, con[0] as $typename);
+            let root1 = Self::find_station_group(&mut group_ids, con[1] as $typename);
             if root0 != root1 {
                 group_ids[root0 as usize] = root1;
             }
         }
 
         for i in 0..c {
-            let root = Solution::find_station_group(&mut group_ids, i as i32);
+            let root = Self::find_station_group(&mut group_ids, i as $typename);
             let entry = groups.entry(root).or_insert(vec![]);
-            entry.push(i as i32);
+            entry.push(i as $typename);
         }
 
 
         for (_, stations) in &mut groups {
             stations.sort_by_key(|&w| std::cmp::Reverse(w));
         }
-
-        *elapsed += now.elapsed();
-
 
         if false {
             println!("groups: ");
@@ -69,12 +68,12 @@ impl Solution {
             println!();
         }
         
-        let mut solutions = Vec::new();
-        let mut down = HashSet::new();
+        let mut solutions:Vec<i32> = Vec::with_capacity(queries.len() / 2);
+        let mut down:HashSet<$typename> = HashSet::new();
         for query in queries {
             if query[0] == 1 {
-                if down.contains(&query[1]) {
-                    let root = Solution::find_station_group(&mut group_ids, query[1]);
+                if down.contains(&(query[1] as $typename)) {
+                    let root = Self::find_station_group(&mut group_ids, query[1] as $typename);
                     let stations = groups.get_mut(&root).expect("....");
 
                     loop {
@@ -82,9 +81,9 @@ impl Solution {
                             solutions.push(-1);
                             break;
                         }
-
-                        if !down.contains(stations.last().expect("...")) {
-                            solutions.push(*stations.last().expect("..."));
+                        let item = stations.last().expect("...");
+                        if !down.contains(item) {
+                            solutions.push(*item as i32);
                             break;
                         }
 
@@ -94,24 +93,43 @@ impl Solution {
                 } else {
                     solutions.push(query[1]);
                 }
-            }
-            if query[0] == 2 {
-                down.insert(query[1]);
+            } else if query[0] == 2 {
+                down.insert(query[1] as $typename);
             }
 
         }
 
         return solutions;
     }
+
+}
+};
+}
+
+
+create_solver!(SolverI32, i32);
+create_solver!(SolverI16, i16);
+
+struct Solution {
+}
+
+impl Solution {
+
+    pub fn process_queries(c: i32, connections: Vec<Vec<i32>>, queries: Vec<Vec<i32>>) -> Vec<i32> {
+        /*if (c < i16::MAX.into()) {
+            return SolverI16::process_queries(c, connections, queries);
+        }*/
+        return SolverI32::process_queries(c, connections, queries);
+    }
 }
 
 fn main() {
-    let mut elapsed = Duration::new(0,0);
+    let now = Instant::now();
 
     let c = 5;
     let connections = vec![vec![1,2],vec![2,3],vec![3,4],vec![4,5]];
     let queries = vec![vec![1,3],vec![2,1],vec![1,1],vec![2,2],vec![1,2]];
-    let sols = Solution::process_queries(c, connections, queries, &mut elapsed);
+    let sols = Solution::process_queries(c, connections, queries);
 
     for sol in sols {
         println!("{:?}", sol);
@@ -120,7 +138,7 @@ fn main() {
     let c = 3;
     let connections = vec![];
     let queries = vec![vec![1,1],vec![2,1],vec![1,1]];
-    let sols = Solution::process_queries(c, connections, queries, &mut elapsed);
+    let sols = Solution::process_queries(c, connections, queries);
 
     for sol in sols {
         println!("{:?}", sol);
@@ -129,7 +147,7 @@ fn main() {
     let c = 3;
     let connections = vec![vec![3,2],vec![1,3],vec![2,1]];
     let queries = vec![vec![2,1]];
-    let sols = Solution::process_queries(c, connections, queries, &mut elapsed);
+    let sols = Solution::process_queries(c, connections, queries);
 
     for sol in sols {
         println!("{:?}", sol);
@@ -143,7 +161,8 @@ fn main() {
             connections.push(vec![i,i+1]);
             queries.push(vec![1,i]);
         }
-        let sols = Solution::process_queries(c, connections, queries, &mut elapsed);
+        let sols = Solution::process_queries(c, connections, queries);
     }
-    println!("Elapsed: {:.2?}", elapsed);
+
+    println!("Elapsed: {:.2?}", now.elapsed());
 }
