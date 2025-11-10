b3af35c7a9e1410133b2413537492199065ed44d
Hannes Verschore <hv1989@gmail.com>
2025-11-10 22:33:37 +0100

    Problem 3607, beats 92% on runtime, 67% on memory - Fix inefficiency in find_station_group (Disjoint Set Union). Save the result to correct group_ids. - Don't sort groups immediately. We only need it when a station goes down. (Which does not alway happen, so we might spare some runtime by not doing it beforehand). - Use usize, instead of i32/i16, less casts - Impoved joining of groups (take the one which has biggest vec already).

--- problem3607/main.rs
+++ problem3607/main.rs
@@ -2,6 +2,8 @@ use std::collections::HashSet;
 use std::collections::HashMap;
 use std::time::Instant;
 use std::time::Duration;
+use std::collections::BinaryHeap;
+use std::cmp::Reverse;
 
 macro_rules! create_solver {
     ($name:ident, $typename:ty) => {
@@ -13,48 +15,39 @@ impl $name
     pub fn find_station_group(group_ids: &mut Vec<$typename>, station: $typename) -> $typename {
         let gid = station as usize;
 
-        if group_ids[gid] == -1 {
-            group_ids[gid] = station;
-            return station;
-        }
-
         if group_ids[gid] == station {
             return station;
         }
 
-        let gid = group_ids[gid];
-        let root = Self::find_station_group(group_ids, gid); 
-        group_ids[gid as usize] = root;
-        return root;
+        group_ids[station] = Self::find_station_group(group_ids, group_ids[station]);
+        return group_ids[station];
     }
 
     pub fn process_queries(c: i32, connections: Vec<Vec<i32>>, queries: Vec<Vec<i32>>) -> Vec<i32> {
         let c = c as usize + 1;
 
-        let mut groups : HashMap<$typename, Vec<$typename>> = HashMap::new();
-        let mut group_ids : Vec<$typename> = Vec::with_capacity(c);
-        group_ids.resize(c, -1);
+        let mut groups : Vec<BinaryHeap<Reverse<$typename>>> = vec![BinaryHeap::new(); c];
+        let mut group_ids : Vec<usize> = (0..c).collect();
+        let mut group_sizes: Vec<usize> = vec![1; c];
 
         for con in connections {
-            let root0 = Self::find_station_group(&mut group_ids, con[0] as $typename);
-            let root1 = Self::find_station_group(&mut group_ids, con[1] as $typename);
+            let mut root0 = Self::find_station_group(&mut group_ids, con[0] as $typename);
+            let mut root1 = Self::find_station_group(&mut group_ids, con[1] as $typename);
             if root0 != root1 {
-                group_ids[root0 as usize] = root1;
+                if group_sizes[root0] < group_sizes[root1] {
+                    std::mem::swap(&mut root0, &mut root1);
+                }
+                group_ids[root1] = root0;
+                group_sizes[root0] += group_sizes[root1];
             }
         }
 
         for i in 0..c {
             let root = Self::find_station_group(&mut group_ids, i as $typename);
-            let entry = groups.entry(root).or_insert(vec![]);
-            entry.push(i as $typename);
+            groups[root].push(Reverse(i as $typename));
         }
 
-
-        for (_, stations) in &mut groups {
-            stations.sort_by_key(|&w| std::cmp::Reverse(w));
-        }
-
-        if false {
+        /*if false {
             println!("groups: ");
             for (gid, stations) in &groups {
                 println!("{}: {:?}", gid, stations);
@@ -66,24 +59,24 @@ impl $name
                 println!("{}: {}", i, group_ids[i]);
             }
             println!();
-        }
+        }*/
         
         let mut solutions:Vec<i32> = Vec::with_capacity(queries.len() / 2);
-        let mut down:HashSet<$typename> = HashSet::new();
+        let mut down:Vec<bool> = vec![false; c];
         for query in queries {
             if query[0] == 1 {
-                if down.contains(&(query[1] as $typename)) {
+                if down[query[1] as usize] {
                     let root = Self::find_station_group(&mut group_ids, query[1] as $typename);
-                    let stations = groups.get_mut(&root).expect("....");
+                    let stations = &mut groups[root];
 
                     loop {
                         if stations.len() == 0 {
                             solutions.push(-1);
                             break;
                         }
-                        let item = stations.last().expect("...");
-                        if !down.contains(item) {
-                            solutions.push(*item as i32);
+                        let item = stations.peek().expect("...").0;
+                        if !down[item as usize] {
+                            solutions.push(item as i32);
                             break;
                         }
 
@@ -94,7 +87,7 @@ impl $name
                     solutions.push(query[1]);
                 }
             } else if query[0] == 2 {
-                down.insert(query[1] as $typename);
+                down[query[1] as usize] = true;
             }
 
         }
@@ -107,8 +100,9 @@ impl $name
 }
 
 
-create_solver!(SolverI32, i32);
-create_solver!(SolverI16, i16);
+//create_solver!(SolverI32, u32);
+//create_solver!(SolverI16, u16);
+create_solver!(SolverI32, usize);
 
 struct Solution {
 }
@@ -116,9 +110,9 @@ struct Solution {
 impl Solution {
 
     pub fn process_queries(c: i32, connections: Vec<Vec<i32>>, queries: Vec<Vec<i32>>) -> Vec<i32> {
-        /*if (c < i16::MAX.into()) {
-            return SolverI16::process_queries(c, connections, queries);
-        }*/
+        //if (c < i16::MAX.into()) {
+        //    return SolverI16::process_queries(c, connections, queries);
+        //}
         return SolverI32::process_queries(c, connections, queries);
     }
 }
